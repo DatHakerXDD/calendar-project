70574fb89891b3140902f9ea153f16d646116852
DatHakerXDD <48974665+DatHakerXDD@users.noreply.github.com>
2020-01-26 23:45:07 -0500

    Update 2
    Problems : 
    Compatibility issues with Date, LocalDate and DateFormat
    Same with Time, LocalTime and TimeFormat
    
    TODO places in code, meaning places to be worked on

--- android-application/MainActivity.kt
+++ android-application/MainActivity.kt
@@ -9,7 +9,13 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import jdk.nashorn.internal.objects.NativeDate.getTime
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalTime
 import java.util.*
 
 
@@ -49,7 +55,7 @@ data class SchoolDay(
 
 data class Holiday(
     var holidayName : String,
-    var holidayDate : Date
+    var holidayDate : DateFormat
 )
 
 var weekDaySchedule: Array<SchoolDay> = Array(5) { SchoolDay() }
@@ -57,17 +63,134 @@ var nineDaySchedule: Array<SchoolDay> = Array(9) { SchoolDay() }
 var periodTypes: MutableList<SchoolPeriod> = mutableListOf(defaultPeriod)
 var periodNames: MutableList<String> = mutableListOf(defaultPeriod.className)
 var holidays: MutableList<Holiday> = mutableListOf()
+@SuppressLint("NewApi")
+var defaultHolidayChoices: MutableList<MutableList<Holiday>> = mutableListOf(mutableListOf(Holiday("List of holidays",LocalDate.of(2000, 1, 1))))
+var defaultHolidayListNames : MutableList<String> = mutableListOf("--- School holiday lists ---")
 var day : Int = 0
 var schoolYears : MutableList<String> = mutableListOf("School Year")
 var schoolName : String = ""
+var schoolYear : Int = 0
+var isItNineDays : Boolean = false
+@SuppressLint("NewApi")
+var periodTimeInfo : Array<LocalTime> = Array(10) {LocalTime.of(12, 0)}
+@SuppressLint("SimpleDateFormat")
+val timeFormatter : DateFormat = SimpleDateFormat("hh:mm:ss a")
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("InflateParams")
+    @SuppressLint("InflateParams", "NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val stLouisHolidays = mutableListOf(
+            Holiday("Demie journée pédagogique", Date(2019, 8, 28)),
+            Holiday("Demie journée pédagogique", LocalDate.of(2019, 8, 29)),
+            Holiday("Fête du travail", LocalDate.of(2019, 9, 2)),
+            Holiday("Journée pédagogique", LocalDate.of(2019, 9, 20)),
+            Holiday("Action de grâce", LocalDate.of(2019, 10, 14)),
+            Holiday("Journée pédagogique", LocalDate.of(2019, 10, 21)),
+            Holiday("Journée pédagogique", LocalDate.of(2019, 11, 1)),
+            Holiday("Journée pédagogique mobile", LocalDate.of(2019, 11, 22)),
+            Holiday("Journée pédagogique", LocalDate.of(2019, 12, 6)),
+            Holiday("Congé de Noël", LocalDate.of(2019, 12, 23)),
+            Holiday("Congé de Noël", LocalDate.of(2019, 12, 24)),
+            Holiday("Noël", LocalDate.of(2019, 12, 25)),
+            Holiday("Congé de Noël", LocalDate.of(2019, 12, 26)),
+            Holiday("Congé de Noël", LocalDate.of(2019, 12, 27)),
+            Holiday("Congé de Noël", LocalDate.of(2019, 12, 30)),
+            Holiday("Congé de Noël", LocalDate.of(2019, 12, 31)),
+            Holiday("Congé de Noël", LocalDate.of(2020, 1, 1)),
+            Holiday("Journée pédagogique", LocalDate.of(2020, 1, 24)),
+            Holiday("Journée pédagogique", LocalDate.of(2020, 2, 12)),
+            Holiday("Journée blanche", LocalDate.of(2020, 2, 20)),
+            Holiday("Journée pédagogique mobile", LocalDate.of(2020, 2, 28)),
+            Holiday("Semaine de relâche", LocalDate.of(2020, 3, 2)),
+            Holiday("Semaine de relâche", LocalDate.of(2020, 3, 3)),
+            Holiday("Semaine de relâche", LocalDate.of(2020, 3, 4)),
+            Holiday("Semaine de relâche", LocalDate.of(2020, 3, 5)),
+            Holiday("Semaine de relâche", LocalDate.of(2020, 3, 6)),
+            Holiday("Journée pédagogique mobile", LocalDate.of(2020, 4, 9)),
+            Holiday("Vendredi saint", LocalDate.of(2020, 4, 10)),
+            Holiday("Lundi de Pâques", LocalDate.of(2020, 4, 13)),
+            Holiday("Journée pédagogique", LocalDate.of(2020, 4, 24)),
+            Holiday("Journée pédagogique", LocalDate.of(2020, 5, 1)),
+            Holiday("Journée verte", LocalDate.of(2020, 5, 27)),
+            Holiday("Journée nationale des patriotes", LocalDate.of(2020, 5, 18)),
+            Holiday("Journée pédagogique", LocalDate.of(2020, 6, 1)),
+            Holiday("Défi endurance", LocalDate.of(2020, 6, 5)))
+
+        defaultHolidayChoices.add(stLouisHolidays)
+        defaultHolidayListNames.add("Collège St-Louis")
+
+        val page1Objects = listOf<View>(schoolNameEt,
+            schoolYearSpn,
+            nineDaySw,
+            mondayBtn,
+            tuesdayBtn,
+            wednesdayBtn,
+            thursdayBtn,
+            fridayBtn,
+            day1Btn,
+            day2Btn,
+            day3Btn,
+            day4Btn,
+            day5Btn,
+            day6Btn,
+            day7Btn,
+            day8Btn,
+            day9Btn,
+            newPeriodBtn,
+            period1spn,
+            period2spn,
+            period3spn,
+            period4spn,
+            period5spn,
+            next1Btn,
+            confirmChk)
+
+        val page2Objects = listOf<View>(
+            back1btn,
+            next2btn,
+            holidayChoiceSpn,
+            addHolidayBtn,
+            holidayTitle1,
+            holidayTitle2,
+            holidayTitle3,
+            holidayTgb1,
+            holidayTgb2
+        )
+
+        val page3Objects = listOf<View>(
+            back2Btn,
+            next3Btn,
+            period1StartEt,
+            period1EndEt,
+            period2StartEt,
+            period2EndEt,
+            period3StartEt,
+            period3EndEt,
+            period4StartEt,
+            period4EndEt,
+            period5StartEt,
+            period5EndEt,
+            period1Time,
+            period2Time,
+            period3Time,
+            period4Time,
+            period5Time,
+            periodStart,
+            periodEnd,
+            schoolYearStartEt,
+            schoolYearEndEt,
+            schoolYearStartTv,
+            schoolYearEndTv
+        )
+
+        holidayChoiceSpn.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            defaultHolidayListNames)
         val thisYear = Calendar.getInstance()[Calendar.YEAR]
         for (i in (1..10)){
             schoolYears.add((thisYear - 6 + i).toString().plus(" - ").plus((thisYear - 5 + i).toString()))
@@ -99,6 +222,7 @@ class MainActivity : AppCompatActivity() {
             listOf(period1spn, period2spn, period3spn, period4spn, period5spn)
 
         nineDaySw.setOnClickListener {
+            isItNineDays = !isItNineDays
             for (i in (dayButtons.indices)) {
                 with(dayButtons[i])
                 {
@@ -124,7 +248,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        //Get and display information of each day on spinners when their buttons are clicked
+
         for (i in (dayButtons.indices)) {
             if (i < 5) {
                 dayButtons[i].setOnClickListener {
@@ -281,9 +405,132 @@ class MainActivity : AppCompatActivity() {
                 }
                 else -> {
                     schoolName = schoolNameEt.text.toString()
+                    schoolYear = schoolYears[schoolYearSpn.selectedItemPosition].take(4).toInt()
+                    for (i in page1Objects.indices){
+                        page1Objects[i].visibility = GONE
+
+                    }
+                    for (i in page2Objects.indices){
+                        page2Objects[i].visibility = VISIBLE
+
+                    }
+                    holidayTgb2.performClick()
                 }
             }
         }
+
+        back1btn.setOnClickListener{
+            for (i in page1Objects.indices){
+                page1Objects[i].visibility = VISIBLE
+
+            }
+            for (i in page2Objects.indices){
+                page2Objects[i].visibility = GONE
+
+            }
+            mondayBtn.performClick()
+        }
+
+        addHolidayBtn.setOnClickListener{
+            Toast.makeText(baseContext, "Coming soon", Toast.LENGTH_SHORT).show()
+            //TODO
+        }
+
+        val holidayToggles = listOf<ToggleButton>(holidayTgb1, holidayTgb2)
+        for (i in holidayToggles.indices){
+            holidayToggles[i].setOnClickListener{
+                holidayToggles[i].isChecked = true
+                holidayToggles[if (i == 1) 0 else 1].isChecked = false
+            }
+        }
+
         mondayBtn.performClick()
+
+        next2btn.setOnClickListener{
+            when {
+                holidayTgb1.isChecked -> {
+                    Toast.makeText(baseContext, "Custom holiday sets coming soon", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(baseContext, "Please choose a default set of holidays", Toast.LENGTH_SHORT).show()
+                }
+                holidayChoiceSpn.selectedItemPosition == 0 -> {
+                    Toast.makeText(baseContext, "Please choose a valid list of holidays", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    for (i in page2Objects.indices){
+                        page2Objects[i].visibility = GONE
+                    }
+                    for (i in page3Objects.indices){
+                        page3Objects[i].visibility = VISIBLE
+                    }
+                }
+            }
+        }
+
+        val periodTimeEditTexts : List<EditText> = listOf(
+            period1StartEt,
+            period1EndEt,
+            period2StartEt,
+            period2EndEt,
+            period3StartEt,
+            period3EndEt,
+            period4StartEt,
+            period4EndEt,
+            period5StartEt,
+            period5EndEt,
+            schoolYearStartEt,
+            schoolYearEndEt
+        )
+
+        var pass = true
+        next3Btn.setOnClickListener{
+            for (i in periodTimeEditTexts.indices){
+                if (periodTimeEditTexts[i].text.toString() == ""){
+                    pass = false
+                }
+            }
+            if (pass){
+                val generateCalendarBuilder = AlertDialog.Builder(this)
+                generateCalendarBuilder.setTitle("Continue?")
+                generateCalendarBuilder.setCancelable(false)
+                generateCalendarBuilder.setMessage("Are you sure you want to continue?")
+                generateCalendarBuilder.setPositiveButton("Continue") { _: DialogInterface, _: Int ->
+                    for (i in (0..9)){
+                        periodTimeInfo[i] = timeFormatter.parse(periodTimeEditTexts[i].text.toString()
+                    }
+                    createCSV()
+                }
+                generateCalendarBuilder.setNegativeButton("Cancel"){ _: DialogInterface, _: Int ->
+                    pass = false
+                }
+                generateCalendarBuilder.show()
+
+            }
+            else{
+                Toast.makeText(baseContext, "Missing required information", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        back2Btn.setOnClickListener{
+            for (i in page2Objects.indices){
+                page2Objects[i].visibility = VISIBLE
+            }
+            for (i in page3Objects.indices){
+                page3Objects[i].visibility = GONE
+            }
+            for (i in (0..9)){
+                //TODO periodTimeInfo[i] = periodTimeEditTexts[i].text.toString().
+            }
+        }
     }
 }
+
+public fun createCSV(){
+
+    val CSVFile = File("CSV Export")
+    for (month in (0..11)){}
+    if (CSVFile.createNewFile()){
+        with(CSVFile){
+            writeText("Subject,Start Date,End Date,All Day Event,Start Time,End Time,Description,Location,Private")
+            writeText("")
+        }
+    }//TODO
